69e941e396dff9d1020bb61e3f0f9ed0358532a0
Zhijian Mei <545902915@qq.com>
2022-11-11 17:30:46 +0800

    Update data_util.py

--- data_util.py
+++ data_util.py
@@ -4,7 +4,7 @@ from torch.utils.data import DataLoader
 
 def get_data(image_size,data_path,batch_size):
     transforms = torchvision.transforms.Compose([
-        torchvision.transforms.Resize(image_size),  # args.image_size + 1/4 *args.image_size
+        torchvision.transforms.Resize(image_size),
         torchvision.transforms.RandomResizedCrop(image_size, scale=(0.8, 1.0)),
         torchvision.transforms.ToTensor(),
         torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
