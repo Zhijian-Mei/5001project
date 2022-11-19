f95142d1bb9c1189ffa15d5867aadf3950aa52b1
Jason-yuj <jasonyu1417@gmail.com>
2022-11-19 16:33:54 +0800

    modify

--- train.py
+++ train.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 import time
 import os
 import copy
-from sklearn.model_selection import train_test_split
 from torch.utils.data import random_split
 from tqdm import tqdm
 
@@ -26,7 +25,8 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Top level data directory. Here we assume the format of the directory conforms
 #   to the ImageFolder structure
-data_dir = "data/Img_crop"
+data_dir = "../data/Img_crop"
+data_val = "../data/Eval"
 
 # Models to choose from [resnet, alexnet, vgg, squeezenet, densenet, inception]
 model_name = "resnet"
@@ -40,6 +40,11 @@ batch_size = 32
 # Number of epochs to train for
 num_epochs = 15
 
+def set_parameter_requires_grad(model, feature_extracting):
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
 
 
 def initialize_model(model_name, num_classes,use_pretrained=True):
@@ -52,7 +57,7 @@ def initialize_model(model_name, num_classes,use_pretrained=True):
         """ Resnet18
         """
         model_ft = models.resnet18(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
+        set_parameter_requires_grad(model_ft, True)
         num_ftrs = model_ft.fc.in_features
         model_ft.fc = nn.Linear(num_ftrs, num_classes)
         input_size = 224
@@ -61,7 +66,7 @@ def initialize_model(model_name, num_classes,use_pretrained=True):
         """ Alexnet
         """
         model_ft = models.alexnet(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
+        set_parameter_requires_grad(model_ft, True)
         num_ftrs = model_ft.classifier[6].in_features
         model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
         input_size = 224
@@ -70,42 +75,10 @@ def initialize_model(model_name, num_classes,use_pretrained=True):
         """ VGG11_bn
         """
         model_ft = models.vgg11_bn(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
+        set_parameter_requires_grad(model_ft, True)
         num_ftrs = model_ft.classifier[6].in_features
         model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
         input_size = 224
-
-    elif model_name == "squeezenet":
-        """ Squeezenet
-        """
-        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
-        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
-        model_ft.num_classes = num_classes
-        input_size = 224
-
-    elif model_name == "densenet":
-        """ Densenet
-        """
-        model_ft = models.densenet121(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
-        num_ftrs = model_ft.classifier.in_features
-        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
-        input_size = 224
-
-    elif model_name == "inception":
-        """ Inception v3
-        Be careful, expects (299,299) sized images and has auxiliary output
-        """
-        model_ft = models.inception_v3(pretrained=use_pretrained)
-        # set_parameter_requires_grad(model_ft, feature_extract)
-        # Handle the auxilary net
-        num_ftrs = model_ft.AuxLogits.fc.in_features
-        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
-        # Handle the primary net
-        num_ftrs = model_ft.fc.in_features
-        model_ft.fc = nn.Linear(num_ftrs,num_classes)
-        input_size = 299
 
     else:
         print("Invalid model name, exiting...")
@@ -119,14 +92,14 @@ model_ft, input_size = initialize_model(model_name, num_classes, use_pretrained=
 # Print the model we just instantiated
 
 dataset = get_dataset(image_size=input_size,data_path=data_dir,batch_size=batch_size)
+dataset_val = get_dataset(image_size=input_size,data_path=data_val,batch_size=batch_size)
 
-train_size = int(0.8 * len(dataset))
-test_size = len(dataset) - train_size
+train_size = int(len(dataset))
 
-train_dataset, eval_dataset = random_split(dataset, [train_size, test_size])
+train_dataset, eval_dataset = random_split(dataset, [train_size, 0])
 
 trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-evalloader = DataLoader(eval_dataset,batch_size=batch_size,shuffle=True)
+evalloader = DataLoader(dataset_val,batch_size=batch_size,shuffle=True)
 
 model_ft.to(device)
 
@@ -166,7 +139,7 @@ for epoch in range(num_epochs):
         running_loss += loss.item() * images.size(0)
         running_corrects += torch.sum(preds == labels.data)
 
-        epoch_acc = running_corrects.double() / len(eval_dataset)
+        epoch_acc = running_corrects.double() / len(dataset_val)
     print(f'epoch{epoch+1} accuracy: ',epoch_acc.item())
     if epoch_acc > best_acc:
         best_acc = epoch_acc
